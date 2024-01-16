891919c28c4e6e42a6bc408b60845d811edfbe50
soumalya70 <soumalya70>
2024-01-16 21:55:02 +0530

    updated views and templates

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',home),
     path("get-blog/<id>",get_blog,name='get_blog'),
+    path("login/",login_attempt,name='login'),
+    path('register/',register_attempt,name='register'),
+    path('show-all-blogs/',show_all_blogs,name='show_all_blogs'),
+    path('create-blogs/',create_blogs,name='create_blogs'),
+    path('update-blogs/',update_blogs,name='update_blogs'),
+    path('delete-blog/<id>/',delete_blog,name='delete_blog')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
+def login_attempt(request):
+    return render(request,'login_attempt.html')
+def register_attempt(request):
+    return render(request,'register_attempt.html')
+def show_all_blogs(request):
+    return render (request,'show_all_blogs.html')
+def create_blogs(request):
+    return render(request,'create_blogs.html')
+def update_blogs(request):
+    return render(request,'update_blogs.html')
+def delete_blog(request,id):
+    return redirect('/')
 def get_blog(request,id):
     context={}
     try:
